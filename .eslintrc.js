70640515e61302a0322af458e394c9634c40ea9d
krzysztof176 <krzysztof176@gmail.com>
2020-10-16 15:33:56 -0400

    Kanban Board [WIP]

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-unresolved': 'off',
+    'max-len': ['error', { code: 150 }],
+    'react/forbid-prop-types': [1, { forbid: [], checkContextTypes: false, checkChildContextTypes: false }],
+    'no-console': 'off',
+  },
+};
